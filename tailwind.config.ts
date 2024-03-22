c7d7f5812901ab6c45fb0a328e5ce466d3ee3fa4
Mickael <mickaelfelizardo2008@gmail.com>
2024-03-22 17:05:42 -0300

    changed font family

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,24 +1,45 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/ui/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-      colors: {
-        background: "#E1EEFF",
-      },
+    content: [
+        './pages/**/*.{js,ts,jsx,tsx,mdx}',
+        './components/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/ui/**/*.{js,ts,jsx,tsx,mdx}',
+        './app/**/*.{js,ts,jsx,tsx,mdx}',
+    ],
+    theme: {
+        extend: {
+            backgroundImage: {
+                'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+                'gradient-conic':
+                    'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+            },
+            colors: {
+                background: '#E1EEFF',
+                royalblue: {
+                    100: '#0360d9',
+                    200: 'rgba(3, 96, 217, 0.31)',
+                },
+                white: '#fff',
+                gray: {
+                    100: '#fafafa',
+                    200: '#021526',
+                },
+                black: '#000',
+                whitesmoke: '#efefef',
+                lavender: '#e1eeff',
+                silver: 'rgba(181, 181, 181, 0.68)',
+                darkslategray: {
+                    100: '#2e2e2e',
+                    200: '#163048',
+                    300: '#0d2b46',
+                },
+                darkgray: '#a8a8a8',
+                aliceblue: '#e6f5fc',
+                lightskyblue: '#a5ccff',
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 };
 export default config;
