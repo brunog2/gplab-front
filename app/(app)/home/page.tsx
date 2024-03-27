93d397e685c02b9538bdd4861c2127ca61aeee1b
Aristoteles Barros <ari@labs-lumi.com.br>
2024-03-27 15:43:11 -0300

    refactor: home text changes

--- app/(app)/home/page.tsx
+++ app/(app)/home/page.tsx
@@ -1,98 +1,90 @@
-import { Button } from '@/src/ui/components/ui/button';
-import { ArrowRight } from 'lucide-react';
-import Image from 'next/image';
-import Link from 'next/link';
+import { Button } from "@/src/ui/components/ui/button";
+import { ArrowRight } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
 
 const ARTICLES = [
-    {
-        title: 'Assunto',
-        content:
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nec risus feugiat lectus risus sed ullamcorper. Auctor semper fermentum volutpat integer vel. In rhoncus elementum nunc, malesuada mi sed. Nibh est sit lobortis id semper.',
-    },
-    {
-        title: 'Assunto',
-        content:
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nec risus feugiat lectus risus sed ullamcorper. Auctor semper fermentum volutpat integer vel. In rhoncus elementum nunc, malesuada mi sed. Nibh est sit lobortis id semper.',
-    },
-    {
-        title: 'Assunto',
-        content:
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nec risus feugiat lectus risus sed ullamcorper. Auctor semper fermentum volutpat integer vel. In rhoncus elementum nunc, malesuada mi sed. Nibh est sit lobortis id semper.',
-    },
+  {
+    title: "Saúde mental",
+    content:
+      "Priorizar a saúde mental é essencial para o bem-estar integral. Cuidar da mente é tão importante quanto cuidar do corpo, promovendo equilíbrio emocional e qualidade de vida. Buscar apoio profissional e cultivar hábitos saudáveis de autocuidado são passos fundamentais para uma vida mentalmente saudável.",
+  },
+  {
+    title: "Doenças cardiovasculares",
+    content:
+      "As doenças cardiovasculares representam uma preocupação global, afetando milhões de vidas a cada ano. Investir na prevenção, através de uma dieta equilibrada, exercícios regulares e controle do estresse, é crucial para proteger o coração e garantir uma vida plena.",
+  },
+  {
+    title: "Recuperação cirúrgica: quanto tempo?",
+    content:
+      "A recuperação pós-cirúrgica é um período crucial que exige cuidados dedicados e apoio adequado. Seguir as orientações médicas, manter uma dieta nutritiva e praticar exercícios leves são passos essenciais para uma recuperação bem-sucedida.",
+  },
 ];
 
 export default function HomePage() {
-    return (
-        <div className="px-16">
-            <div>
-                <h3 className="font-bold text-4xl text-blue-700">
-                    Seja bem vindo(a)!
-                </h3>
+  return (
+    <div className="px-16">
+      <div>
+        <h3 className="font-bold text-4xl text-blue-700">Seja bem vindo(a)!</h3>
 
-                <div>
-                    <h4 className="font-bold text-3xl text-blue-700 mt-14">
-                        Como podemos te auxiliar hoje?
-                    </h4>
+        <div>
+          <h4 className="font-bold text-3xl text-blue-700 mt-14">
+            Como podemos te auxiliar hoje?
+          </h4>
 
-                    <span className="flex gap-2 px-4 rounded-full bg-gray-200 overflow-hidden mt-8">
-                        <input
-                            type="text"
-                            className="appearance-none bg-transparent focus:outline-none w-full px-2 py-4 text-lg"
-                        />
-                        <button className="bg-blue-700 my-4 rounded-full p-4 text-white">
-                            <ArrowRight />
-                        </button>
-                    </span>
-                </div>
-            </div>
-
-            <section className="mt-14">
-                <div className="flex flex-col xl:flex-row gap-20">
-                    <div className="flex-1 pb-10">
-                        <h4 className="font-bold text-3xl text-blue-700">
-                            Novidades
-                        </h4>
-
-                        <div className="mt-6 space-y-4">
-                            {ARTICLES.map((article, index) => (
-                                <div key={index}>
-                                    <article>
-                                        <h5 className="font-semibold text-xl text-blue-700">
-                                            {article.title}
-                                        </h5>
-                                        <p className="mt-1">
-                                            {article.content}
-                                        </p>
-                                    </article>
-                                </div>
-                            ))}
-                        </div>
-                    </div>
-                    <div>
-                        <div>
-                            <h4 className="font-bold text-3xl text-blue-700 mb-4">
-                                Agende sua consulta agora mesmo
-                            </h4>
-                            <Link href={''}>
-                                <Button className="w-full">
-                                    Agendar consulta
-                                </Button>
-                            </Link>
-                        </div>
-                        <div className="mt-6">
-                            <h4 className="font-bold text-3xl text-blue-700 mb-4">
-                                Verificar consultas agendadas
-                            </h4>
-                            <Link href={''}>
-                                <Button className="w-full">Consulta</Button>
-                            </Link>
-                        </div>
-                        <span className="w-full block relative aspect-[347/196]">
-                            <Image src={'/doctors@2x.png'} alt="doctors" fill />
-                        </span>
-                    </div>
-                </div>
-            </section>
+          <span className="flex gap-2 px-4 rounded-full bg-gray-200 overflow-hidden mt-8">
+            <input
+              type="text"
+              className="appearance-none bg-transparent focus:outline-none w-full px-2 py-4 text-lg"
+            />
+            <button className="bg-blue-700 my-4 rounded-full p-4 text-white">
+              <ArrowRight />
+            </button>
+          </span>
         </div>
-    );
+      </div>
+
+      <section className="mt-14">
+        <div className="flex flex-col xl:flex-row gap-20">
+          <div className="flex-1 pb-10">
+            <h4 className="font-bold text-3xl text-blue-700">Novidades</h4>
+
+            <div className="mt-6 space-y-4">
+              {ARTICLES.map((article, index) => (
+                <div key={index}>
+                  <article>
+                    <h5 className="font-semibold text-xl text-blue-700">
+                      {article.title}
+                    </h5>
+                    <p className="mt-1">{article.content}</p>
+                  </article>
+                </div>
+              ))}
+            </div>
+          </div>
+          <div>
+            <div>
+              <h4 className="font-bold text-3xl text-blue-700 mb-4">
+                Agende sua consulta agora mesmo
+              </h4>
+              <Link href={""}>
+                <Button className="w-full">Agendar consulta</Button>
+              </Link>
+            </div>
+            <div className="mt-6">
+              <h4 className="font-bold text-3xl text-blue-700 mb-4">
+                Verificar consultas agendadas
+              </h4>
+              <Link href={""}>
+                <Button className="w-full">Consulta</Button>
+              </Link>
+            </div>
+            <span className="w-full block relative aspect-[347/196]">
+              <Image src={"/doctors@2x.png"} alt="doctors" fill />
+            </span>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 }
--- src/ui/components/footer/footer.tsx
+++ src/ui/components/footer/footer.tsx
@@ -1,54 +1,50 @@
-import Image from 'next/image';
-import Link from 'next/link';
+import Image from "next/image";
+import Link from "next/link";
 
 export const Footer: React.FC = () => {
-    return (
-        <footer className="bg-blue-700 text-white">
-            <div className="container mx-auto px-4 py-20">
-                <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-x-36 gap-y-14">
-                    <div className="col-span-2">
-                        <h4 className="font-semibold text-2xl">GPLab</h4>
-                        <p className="mt-8">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing
-                            elit. Nec risus feugiat lectus risus sed
-                            ullamcorper. Auctor semper fermentum volutpat
-                            integer vel. In rhoncus elementum nunc, malesuada mi
-                            sed. Nibh est sit lobortis id semper.
-                        </p>
-                    </div>
-                    <div className="col-span-2 md:col-span-1">
-                        <h4 className="font-semibold text-2xl">Useful Links</h4>
-                        <ul className="mt-8 font-medium">
-                            <li>
-                                <Link href={''}>About us</Link>
-                            </li>
-                            <li>
-                                <Link href={''}>Privacy Policy</Link>
-                            </li>
-                            <li>
-                                <Link href={''}>Our Mission</Link>
-                            </li>
-                            <li>
-                                <Link href={''}>Our Team</Link>
-                            </li>
-                        </ul>
-                    </div>
-                    <div className="col-span-2">
-                        <h4 className="font-semibold text-2xl">Addresss</h4>
-                        <Image
-                            src={'/rectangle-18@2x.png'}
-                            alt="map"
-                            className="mt-8"
-                            width={366}
-                            height={160}
-                        />
-                    </div>
-                </div>
-            </div>
-            <hr className="border-white container mx-auto px-4" />
-            <span className="block py-6 text-center">
-                © 2024 All Right Reserved
-            </span>
-        </footer>
-    );
+  return (
+    <footer className="bg-blue-700 text-white">
+      <div className="container mx-auto px-4 py-20">
+        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-x-36 gap-y-14">
+          <div className="col-span-2">
+            <h4 className="font-semibold text-2xl">GPLab</h4>
+            <p className="mt-8">
+              Com GPLab, sua jornada pela saúde se torna mais fácil e acessível
+              - agende suas consultas médicas com praticidade e tranquilidade,
+              colocando sua saúde em boas mãos.
+            </p>
+          </div>
+          <div className="col-span-2 md:col-span-1">
+            <h4 className="font-semibold text-2xl">Links úteis</h4>
+            <ul className="mt-8 font-medium">
+              <li>
+                <Link href={""}>Sobre nós</Link>
+              </li>
+              <li>
+                <Link href={""}>Nossa política</Link>
+              </li>
+              <li>
+                <Link href={""}>Nossa missão</Link>
+              </li>
+              <li>
+                <Link href={""}>Nossa equipe</Link>
+              </li>
+            </ul>
+          </div>
+          <div className="col-span-2">
+            <h4 className="font-semibold text-2xl">Endereço</h4>
+            <Image
+              src={"/rectangle-18@2x.png"}
+              alt="map"
+              className="mt-8"
+              width={366}
+              height={160}
+            />
+          </div>
+        </div>
+      </div>
+      <hr className="border-white container mx-auto px-4" />
+      <span className="block py-6 text-center">© 2024 All Right Reserved</span>
+    </footer>
+  );
 };
