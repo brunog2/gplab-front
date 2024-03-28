7b55290784508b05409aff6200dab40d9a8bd457
Bruno Gomes <bgbf@ic.ufal.br>
2024-03-28 07:11:29 -0300

    feat: axios

--- src/data/actions/UserActions.ts
+++ src/data/actions/UserActions.ts
@@ -1,39 +1,39 @@
-'use server';
+"use server";
 
-import { UserFormSchema } from '@/src/ui/forms/UserForm';
+import { api } from "@/src/services/api";
+import { UserFormSchema } from "@/src/ui/forms/UserForm";
 
 interface actionReturnType {
-    errors?: { [k in keyof UserFormSchema]?: string };
+  errors?: { [k in keyof UserFormSchema]?: string };
 }
 
 export async function createUserAction(
-    prev: any,
-    formData: FormData
+  prev: any,
+  formData: FormData
 ): Promise<actionReturnType> {
-    const data = Object.fromEntries(formData);
-
-    console.log(data);
-
-    return {};
+  const data = Object.fromEntries(formData);
+  const response = await api.post("user/profile", data);
+  console.log(data, response);
+  return response.data;
 }
 
 export async function updateUserAction(
-    prev: any,
-    formData: FormData
+  prev: any,
+  formData: FormData
 ): Promise<actionReturnType> {
-    const data = Object.fromEntries(formData);
+  const data = Object.fromEntries(formData);
 
-    console.log(data);
+  console.log(data);
 
-    return {};
+  return {};
 }
 
 export async function deleteUserAction(
-    formData: FormData
+  formData: FormData
 ): Promise<actionReturnType> {
-    const data = Object.fromEntries(formData);
+  const data = Object.fromEntries(formData);
 
-    console.log(data.id);
+  console.log(data.id);
 
-    return {};
+  return {};
 }
